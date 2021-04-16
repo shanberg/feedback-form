82bed09d2b5aec00a3f7305fe6d603e9a6da6333
shanberg <98312+shanberg@users.noreply.github.com>
2021-04-16 12:33:23 -0400

    feat: improved component

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,25 @@
 <svelte:options tag="feedback-form" />
 
 <script>
-  import { fade } from "svelte/transition";
-  export let showFeedbackDialog = false;
+  export let showFeedbackDialog = true;
   export let callback = () => null;
 
   // Default contents
   export let defaultApp = "drive-ui";
   export let defaultEmail = "";
   export let defaultBody = "";
-  export let defaultMessageType = "asdf";
+  export let defaultMessageType = "";
 
   // Default labels, messages, etc.
   export let emailLabel = "Email Address";
-  export let emailPlaceholder = "phil.lee@greymatter.io";
-  export let emailHelp = "probably includes an '@' symbol";
+  export let emailPlaceholder = "name@provider";
+  export let emailHelp = "We'll only contact you about this feedback";
 
   export let bodyLabel = "Message";
-  export let bodyHelp = "Please be helpful and considerate";
-  export let bodyPlaceholder = "Please add your cool message";
+  export let bodyHelp = "";
+  export let bodyPlaceholder = "";
 
-  export let submitButtonLabel = "Submit Feedback";
+  export let submitButtonLabel = "";
   export let thanksMessage = "Thanks for submitting your feedback!";
 
 	// Message types and options
@@ -32,11 +31,14 @@
     {
       messageType: "bug-report",
       label: "Report Issue",
-			bodyHelp: 'Include any steps to reproduce'
+			submitButtonLabel: "Send Report",
+			bodyHelp: 'Please include steps to reproduce the problem, your operating system, what happened, and what you expected to happen.'
     },
     {
       messageType: "feature-request",
       label: "Request Feature",
+			submitButtonLabel: "Send Request",
+			bodyHelp: 'What is the feature? Why is this feature needed?'
     },
   ];
 
@@ -64,7 +66,7 @@
   $: _bodyPlaceholder =
     currentOpt.bodyPlaceholder || bodyPlaceholder || undefined;
   $: _submitButtonLabel =
-    currentOpt.submitButtonLabel || submitButtonLabel || "Submit Feedback";
+    currentOpt.submitButtonLabel || submitButtonLabel || "Send Feedback";
 	$: canSubmitButton = body !== '';
 
   // Functions
@@ -114,8 +116,7 @@
 
 {#if showFeedbackDialog}
   <main
-    class="feedback-dialog"
-    transition:fade={{ duration: transitionDuration }}
+    class="feedback-dialog feedback"
   >
     <header>
       <h1>Feedback</h1>
@@ -131,13 +132,14 @@
         {/each}
       </div>
       <input type="hidden" value={app} />
+      <input type="hidden" value={window.navigator.userAgent} />
       <label
-        ><span>{_emailLabel}</span>
+        ><span class="label">{_emailLabel}</span>
         <input type="text" placeholder={_emailPlaceholder} bind:value={email} />
         {#if _emailHelp}<span class="help">{_emailHelp}</span>{/if}
       </label>
       <label
-        ><span>{_bodyLabel}</span>
+        ><span class="label">{_bodyLabel}</span>
         <textarea
           rows="10"
           placeholder={_bodyPlaceholder}
@@ -157,14 +159,68 @@
 
 {#if showingThanksMessage}
   <div
-    transition:fade={{ duration: transitionDuration }}
-    class="thanks-message"
+    class="thanks-message feedback"
   >
     {_thanksMessage}
   </div>
 {/if}
 
 <style>
+	.feedback {
+
+    --_color-background: var(--color-background, #fff);
+    --_color-border: var(--color-border, rgba(0,0,0,0.08));
+    --_color-shadow: var(--color-shadow, rgba(0,0,0,0.1));
+    --_color-highlight: var(--color-highlight, blue);
+    --_color-body-text: var(--color-body-text, #000);
+    --_color-body-text---muted: var(--color-body-text---muted, rgba(0, 0, 0, 0.7));
+
+		--_shadow: var(--shadow, 0 0.125rem 0.25rem var(--_color-shadow));
+		--_font-size: var(--font-size, 1rem);
+
+    --_control-margin: var(--control-margin, 0.25rem);
+    --_control-padding: var(--control-padding, 0.25rem 0.5rem);
+    --_control-border-radius: var(--control-border-radius, 0.25rem);
+
+    --_control-background: var(--control-background, #ddd);
+    --_control-text: var(--control-background, #000);
+    --_control-border: var(--control-border, 0);
+
+    --_control-background---current: var(--control-background, var(--_color-highlight));
+    --_control-color---current: var(--control-background, #fff);
+    --_control-border---current: var(--control-border, 0);
+
+    --_control-background---current: var(--control-background, var(--_color-highlight));
+    --_control-color---current: var(--control-background, #fff);
+    --_control-border---current: var(--control-border, 0);
+
+		--_field-background: var(--_field-background, var(--_color-background));
+		--_field-border: var(--field-border, 1px solid var(--_color-border));
+		--_field-color: var(--field-color, var(--_color-body-text));
+		--_field-padding: var(--field-padding, var(--_control-padding));
+
+		--_field-background---current: var(--_field-background---current, var(--_color-background));
+		--_field-border---current: var(--field-border---current, 1px solid var(--_color-highlight));
+		--_field-color---current: var(--field-color---current, var(--_color-body-text));
+
+		--_form-shadow: var(--form-shadow, var(--_shadow));
+    --_form-outer-spacing: var(--form-outer-spacing, 1rem);
+    --_form-inner-spacing: var(--form-inner-spacing, 1rem);
+    --_form-max-width: var(--form-max-width, 21em);
+    --_form-border-radius: var(--form-border-radius, 0.5rem);
+    --_form-background: var(--form-background, #fff);
+    --_form-border: var(--form-border, 1px solid var(--_color-border));
+    --_form-shadow: var(--form-shadow, 0 0.5rem 0.5rem var(--_color-shadow));
+    --_form-font-family: var(--form-font-family, inherit);
+
+		--_thanks-shadow: var(--thanks-shadow, var(--_shadow));
+		--_thanks-font-family: var(--thanks-font-family, inherit);
+		--_thanks-background: var(--thanks-background, var(--_color-background));
+		--_thanks-color: var(--thanks-color, var(--_color-body-text));
+		--_thanks-padding: var(--thanks-padding, 1rem);
+		--_thanks-border-radius: var(--thanks-border-radius, 0.5rem);
+  }
+
   @keyframes appear {
     from {
       opacity: 0;
@@ -191,58 +247,40 @@
     border: 1px solid blue;
   }
 
-  .feedback-dialog,
-  .thanks-message {
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  }
 
-  .thanks-message {
-    --background: #fff;
-  }
+	.feedback-dialog,
+	.thanks-message {
+		position: fixed;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
 
-  main {
-    --_color-background: var(--color-background, #fff);
-    --_color-border: var(--color-border, #eee);
-    --_color-shadow: var(--color-shadow, #000);
-    --_color-body-text: var(--color-body-text, #000);
-    --_color-body-text---muted: var(--color-body-text---muted, rgba(0, 0, 0, 0.7));
-
-    --_control-margin: var(--control-margin, 0.25rem);
-    --_control-padding: var(--control-padding, 0.25rem 0.5rem);
-    --_control-border-radius: var(--control-border-radius, 0.25rem);
-
-    --_control-background: var(--control-background, #ddd);
-    --_control-text: var(--control-background, #000);
-    --_control-border: var(--control-border, 0);
-
-    --_control-background---current: var(--control-background, #blue);
-    --_control-color---current: var(--control-background, #fff);
-    --_control-border---current: var(--control-border, 0);
-
-    --_font-size: var(--font-size, 1rem);
-    --_form-outer-spacing: var(--form-outer-spacing, 1rem);
-    --_form-inner-spacing: var(--form-inner-spacing, 1rem);
-    --_form-max-width: var(--form-max-width, 20em);
-    --_form-border-radius: var(--form-border-radius, 0.5rem);
-    --_form-background: var(--form-background, #fff);
-    --_form-border: var(--form-border, 1px solid);
-    --_form-font-family: var(--form-font-family, inherit);
-
-    max-height: calc(100vh - (var(--_form-outer-spacing) / 2));
-    max-width: calc(100vw - (var(--_form-outer-spacing) / 2));
-    display: flex;
-    flex-direction: column;
-    align-items: stretch;
+	.feedback-dialog {
+		box-shadow: var(--_form-shadow);
     font-family: var(--_form-font-family);
     border-radius: var(--_form-border-radius);
     border: var(--_form-border);
-    max-width: var(--_form-max-width);
+    width: var(--_form-max-width);
     padding: var(--_form-inner-spacing);
     animation: appear 1s both;
-  }
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    max-height: calc(100vh - (var(--_form-outer-spacing) / 2));
+    max-width: calc(100vw - (var(--_form-outer-spacing) / 2));
+	}
+
+	.thanks-message {
+		--background: #fff;
+		background: var(--_thanks-background);
+		box-shadow: var(--_thanks-shadow);
+		font-family: var(--_thanks-font-family);
+		padding: var(--_thanks-padding);
+		border-radius: var(--_thanks-border-radius);
+		width: max-content;
+		max-width: 90%;
+	}
 
 	header {
 
@@ -266,18 +304,33 @@
   .form {
     display: flex;
     flex-direction: column;
-    gap: 1rem;
   }
 
   label {
     display: flex;
     flex-direction: column;
     align-items: stretch;
+		margin-top: 0.5rem;
+		margin-bottom: 0.5rem;
   }
 
-	label input {
-		margin-top: 0.25rem;
+	input,
+	textarea {
+		margin-top: 0.125rem;
 		margin-bottom: 0.25rem;
+		padding: var(--_field-padding);
+		font: inherit;
+
+		background: var(--_field-background);
+		color: var(--_field-color);
+		border: var(--_field-border);
+	}
+
+	label input:focus,
+	label textarea:focus {
+		background: var(--_field-background---current);
+		color: var(--_field-color---current);
+		border: var(--_field-border---current);
 	}
 
   label span {
@@ -293,16 +346,24 @@
   input,
   textarea {
     border-radius: var(--_control-border-radius);
-    border: 1px solid;
   }
 
   .type-picker {
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
+		margin: -0.25rem;
+		justify-content: space-between;
+		padding-bottom: 0.5rem;
+		border-bottom: 1px solid var(--_color-border);
+		margin-bottom: 0.5rem;
   }
 
-  .type-picker button {
+	.type-picker button {
+		margin: 0.25rem;
+	}
+
+  button {
     appearance: none;
     background: var(--_control-background);
     padding: var(--_control-padding);
@@ -310,12 +371,54 @@
     border: var(--_control-border);
   }
 
-  .type-picker .current {
-    background: blue;
-    color: white;
+  button.current {
+    background: var(--_control-background---current);
+    border: var(--_control-border---current);
+    color: var(--_control-color---current);
   }
 
-  .type-picker button:hover {
+	/* Extra Styles */
+	/* */
+	/* */
+	/* */
+	/* */
+	/* */
+	/* */
+	/* */
+	/* */
+	/* */
+	/* */
+	/* */
+	input,
+	textarea {
+		border: 1px solid rgba(0,0,0,0.08);
+		background: rgba(0,0,0,0.02);
+		background-clip: padding-box;
+		transition: border 0.1s ease;
+	}
+
+	label .label {
+		font-size: 70%;
+		font-weight: 600;
+		letter-spacing: 0.06em;
+		text-transform: uppercase;
+		color: var(--_color-body-text---muted);
+	}
+
+	input:focus,
+	textarea:focus {
+		outline: none;
+	}
+
+
+  button:hover {
     filter: brightness(110%);
   }
+
+
+	.feedback-dialog {
+		--color-highlight: #00b42b;
+		--color-border: rgba(0,0,0,0.08);
+	}
+
 </style>
